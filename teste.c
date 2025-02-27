4b3c2359eb7fc1afe837026c4aebe7147a57f82b
Filippe F Oliveira <filippeoliveira@gmail.com>
2025-02-27 20:05:42 -0300

    final

--- teste.c
+++ teste.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
-#include <ctype.h>  
+#include <stdio.h>
+#include <ctype.h>
 
 int main() {
     printf("Vamos criar as cartas para um jogo? Digite as informações solicitadas abaixo: \n");
-    
-    // Cadastro da primeira carta
+
+    // Primeira carta
     char estado1, codigoCarta1[10], nomeCidade1[50];
     int populacao1, pontosTuristicos1;
-    float area1;
-    double pib1; // Usando double para maior precisão
+    float area1, pib1;
+    
 
     printf("Vamos para a primeira carta: \n");
 
@@ -16,7 +16,7 @@ int main() {
     scanf(" %c", &estado1);
     estado1 = toupper(estado1);
 
-    printf("Digite o número de 1 a 4 começando com 0: \n");
+    printf("Digite o numero de 1 a 4 começando com 0: \n");
     scanf("%s", codigoCarta1);
 
     printf("Digite o nome da cidade: \n");
@@ -29,7 +29,7 @@ int main() {
     scanf("%f", &area1);
 
     printf("Digite o PIB desta cidade (em bilhões, ex: 699.28): \n");
-    scanf("%lf", &pib1); // Correção: %lf para double
+    scanf("%.2f", &pib1); // Usando %lf para ler double
 
     printf("Digite a quantidade de pontos turísticos desta cidade: \n");
     scanf("%d", &pontosTuristicos1);
@@ -37,23 +37,23 @@ int main() {
     // Converter PIB de bilhões para reais
     pib1 *= 1e9;
 
-    // Cadastro da segunda carta
+    // Segunda carta
     char estado2, codigoCarta2[10], nomeCidade2[50];
     int populacao2, pontosTuristicos2;
     float area2;
-    double pib2;
+    double pib2; // Alterado para double para maior precisão
 
-    printf("\nVamos para a segunda carta: \n");
+    printf("Vamos para a segunda carta: \n");
 
     printf("Digite uma Letra: \n");
     scanf(" %c", &estado2);
     estado2 = toupper(estado2);
 
-    printf("Digite o número de 1 a 4 começando com 0: \n");
+    printf("Digite o numero de 1 a 4 começando com 0: \n");
     scanf("%s", codigoCarta2);
 
     printf("Digite o nome da cidade: \n");
-    scanf(" %[^\n]", nomeCidade2);  // Correção: espaço antes de %[^\n]
+    scanf(" %[^\n]", nomeCidade2);
 
     printf("Digite a população desta cidade: \n");
     scanf("%d", &populacao2);
@@ -62,7 +62,7 @@ int main() {
     scanf("%f", &area2);
 
     printf("Digite o PIB desta cidade (em bilhões, ex: 699.28): \n");
-    scanf("%lf", &pib2); // Correção: %lf para double
+    scanf("%.2f", &pib2); // Usando %lf para ler double
 
     printf("Digite a quantidade de pontos turísticos desta cidade: \n");
     scanf("%d", &pontosTuristicos2);
@@ -80,10 +80,15 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1 / 1e9);
     printf("Pontos Turísticos: %d\n", pontosTuristicos1);
     printf("Densidade Populacional: %.2f hab/km²\n", populacao1 / area1);
-    printf("PIB per Capita: %.2f reais\n", pib1 / populacao1);
+
+    /*if (populacao1 > 0) {*/
+        printf("PIB per Capita: %.2f reais\n", pib1 / populacao1);
+    /*} else {
+        printf("PIB per Capita: N/A (população inválida)\n");
+    }*/
 
     // Exibir informações da segunda carta
-    printf("\n=== Carta 02 ===\n");
+    /*printf("\n=== Carta 02 ===\n");
     printf("Estado: %c\n", estado2);
     printf("Código da Carta: %c%s\n", estado2, codigoCarta2);
     printf("Nome da Cidade: %s\n", nomeCidade2);
@@ -92,7 +97,12 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib2 / 1e9);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
     printf("Densidade Populacional: %.2f hab/km²\n", populacao2 / area2);
-    printf("PIB per Capita: %.2f reais\n", pib2 / populacao2);
-    
+
+    if (populacao2 > 0) {
+        printf("PIB per Capita: %.2f reais\n", pib2 / populacao2);
+    } else {
+        printf("PIB per Capita: N/A (população inválida)\n");
+    }*/
+
     return 0;
 }
